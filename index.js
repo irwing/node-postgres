e7ce8448336d556191c85d48f5acc3cf6d0c675b
Irwing Naranjo <khalisser@gmail.com>
2021-10-20 14:07:37 -0300

    Hello world with express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Running");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hi!')
+})
+
+app.listen(port, () => {
+  console.log(`Running in port: ${port}`);
+})
