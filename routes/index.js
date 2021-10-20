ad65e94f38d366cde91c93270bdded824d099391
Irwing Naranjo <khalisser@gmail.com>
2021-10-20 15:21:14 -0300

    Adding route prefix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
+const express = require('express');
+
 const usersRouter = require('./users.router');
 
 function routerApi(app) {
-  app.use('/users', usersRouter);
+
+  const router = express.Router();
+
+  app.use('/api/v1', router);
+  router.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
