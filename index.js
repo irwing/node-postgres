1dca64cb18c8a7819bd531d4d829bcab8b1291c5
Irwing Naranjo <khalisser@gmail.com>
2021-11-10 17:53:57 -0300

    Install morgan

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 // use middleware to control the ips that access
 app.use(cors());
