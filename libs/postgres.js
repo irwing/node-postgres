8856c0fed40567bb4b335c03c444e604875bf2ec
Irwing Naranjo <khalisser@gmail.com>
2021-11-10 19:53:31 -0300

    Environment config

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+const { config } = require('./../config/config');
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5431,
-  user: 'postgres',
-  password: 'postgres',
-  database: 'users',
-});
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:`+
+            `${PASSWORD}@${config.dbHost}:`+
+            `${config.dbPort}/${config.dbName}`;
+
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
